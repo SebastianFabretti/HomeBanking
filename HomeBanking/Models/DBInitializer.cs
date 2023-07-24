d5a7a932dd8cd4b8f44fa0571d3241daca68cab5
Sebb <sebastianfabretti@gmail.com>
2023-07-24 18:26:37 -0300

    Agregar archivos de proyecto.

---
+++ HomeBanking/Models/DBInitializer.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel;
+using System.Linq;
+
+namespace HomeBanking.Models
+{
+    public class DBInitializer
+    {
+        public static void Initialize(HomeBankingContext context)
+        {
+            if (!context.Clients.Any())
+            {
+                Client[] clients = new Client[]
+                {
+                   new Client
+                   {
+                       FirstName="Seba",
+                       LastName="Fabre",
+                       Email = "seba@gmail.com",
+                       Password="1111"
+                   },
+                   new Client
+                   {
+                       FirstName="Sebas",
+                       LastName="Fabres",
+                       Email = "sebas@gmail.com",
+                       Password="2222"
+                   },
+                };
+
+                foreach (Client client in clients)
+                {
+                    context.Clients.Add(client);
+                }
+                context.SaveChanges();
+            }
+        }
+    }
+}
