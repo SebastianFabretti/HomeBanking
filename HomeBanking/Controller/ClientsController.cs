2e8b7e2ee26c3850726efdd64fc6424d488dece3
Sebb <sebastianfabretti@gmail.com>
2023-07-26 13:21:36 -0300

    Added ClientsController

---
+++ HomeBanking/Controller/ClientsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HomeBanking.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientsController : ControllerBase
+    {
+
+    }
+}
