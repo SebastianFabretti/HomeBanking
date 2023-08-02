e006870f0d51e5ad633aa53aa005c66d3be94478
Sebb <sebastianfabretti@gmail.com>
2023-08-02 09:19:52 -0300

    Created card class

---
+++ HomeBanking/Models/Card.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Razor.Language.Extensions;
+using System;
+using System.Globalization;
+
+namespace HomeBanking.Models
+{
+    public class Card
+    {
+        public long Id { get; set; }
+        public string CardHolder { get; set; }
+        public string Tyoe { get; set; }
+        public string Color { get; set; }
+        public string Number { get; set; }
+        public int Cvv { get; set; }
+        public DateTime FromDate { get; set; }
+        public DateTime ThruDate { get; set; }
+        public long ClientId { get; set; }
+        public Client Client { get; set; }
+
+    }
+}
--- HomeBanking/Models/HomeBankingContext.cs
+++ HomeBanking/Models/HomeBankingContext.cs
@@ -13,5 +13,6 @@ namespace HomeBanking.Models
         public DbSet<Transaction> Transactions { get; set; }
         public DbSet<Loan> Loans { get; set; }
         public DbSet<ClientLoan> ClientLoans { get; set; }
+        public DbSet<Card> Cards { get; set; }
     }
 }
